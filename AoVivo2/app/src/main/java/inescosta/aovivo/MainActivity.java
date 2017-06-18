9297f5dfca5ec1abcfc2f27f5b35f9c92666490a
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-06-18 17:56:57 +0100

    trying to integrate google maps

--- AoVivo2/app/src/main/java/inescosta/aovivo/MainActivity.java
+++ AoVivo2/app/src/main/java/inescosta/aovivo/MainActivity.java
@@ -1,6 +1,7 @@
 package inescosta.aovivo;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -9,14 +10,21 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private TextView mTextMessage;
     private ImageView apphome;
     private ImageView info;
-    private MapView map;
+    private SupportMapFragment mapFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,18 +34,16 @@ public class MainActivity extends AppCompatActivity {
 
             apphome.setVisibility(View.GONE);
             info.setVisibility(View.GONE);
-            map.setVisibility(View.GONE);
 
             switch (item.getItemId()) {
-                case R.id.navigation_home: // info
+                case R.id.navigation_home:          // info
                     info.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard: // dash
+                case R.id.navigation_dashboard:     // dash
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications: // map
-                    map.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -46,23 +52,51 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TEXT
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        apphome = (ImageView) findViewById(R.id.imageView3);
 
+        //APP HOME
+        apphome = (ImageView) findViewById(R.id.imageView3);
+        apphome.setVisibility(View.VISIBLE);
+
+        apphome.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                apphome.setVisibility(View.GONE);
+                BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+                navigation.setSelectedItemId(R.id.navigation_dashboard);
+            }
+        });
+
+        // INFO
         info = (ImageView) findViewById(R.id.info);
         info.setVisibility(View.GONE);
 
-        map = (MapView) findViewById(R.id.mapView);
-        map.setVisibility(View.GONE);
+        // MAP
 
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
